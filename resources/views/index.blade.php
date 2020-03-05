5ac6c06217ad9f1256450de51d85122ac1f32ef1
Agus Michael Sianipar <43785502+agusmichaelsianipar@users.noreply.github.com>
2020-03-05 18:22:30 +0700

    Login form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,23 @@
 @extends('layout/main')
+
+@section('title','SISTEM INFORMASI BIMBINGAN TUGAS AKHIR')
+
+@section('containing')
+    <div class="kotak_login">
+        <p class="login_label">SIBITA LOGIN</p>
+        <form>
+            <div class="form-group">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                <small id="emailHelp" class="form-text text-muted">Make sure you're email is a registered email.</small>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <br>
+            <center>
+                <button type="submit" class="btn btn-primary">LOGIN</button>
+            </center>
+        </form>
+        <p class="label_l_password"><a href="#" class="label_l_password">Lupa Password</a></p>
+    </div>
+    @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,17 +1,26 @@
 <html>
     <head>
-    <link rel="stylesheet" type="text/css" href="{!! asset('assets/css/style.css') !!}">
+        <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{!! asset('assets/css/berandastyle.css') !!}">
     </head>
     <body>
         <div class="container">
             <div class="logo">
-                <img src="{!! asset('assets/image/logo-itera.png') !!}" alt="">
+                <img class="atas" src="{!! asset('assets/image/logo-itera.png') !!}" alt="">
             </div>
             <div class="judul">
-            <p>SISTEM INFORMASI BIMBINGAN TUGAS AKHIR</p>
-            <p>PROGRAM STUDI TEKNIK INFORMATIKA</p>
-            <p>INSTITUT TEKNOLOGI SUMATERA</p>
+            <p class="top-title">SISTEM INFORMASI BIMBINGAN TUGAS AKHIR</p>
+            <p class="top-title-2">PROGRAM STUDI TEKNIK INFORMATIKA</p>
+            <p class="top-title-3">INSTITUT TEKNOLOGI SUMATERA</p>
             </div>
         </div>
+
+        <div class="navbarberanda">
+
+        </div>
+
+            @yield('containing')
+
     </body>
 </html>
