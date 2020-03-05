3de4e785da7e50243a39ae3ef5965546c80ee923
Agus Michael Sianipar <43785502+agusmichaelsianipar@users.noreply.github.com>
2020-03-05 11:22:36 +0700

    Framework Laravel

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="{!! asset('assets/css/style.css') !!}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="logo">
+                <img src="{!! asset('assets/image/logo-itera.png') !!}" alt="">
+            </div>
+            <div class="judul">
+            <p>SISTEM INFORMASI BIMBINGAN TUGAS AKHIR</p>
+            <p>PROGRAM STUDI TEKNIK INFORMATIKA</p>
+            <p>INSTITUT TEKNOLOGI SUMATERA</p>
+            </div>
+        </div>
+    </body>
+</html>
