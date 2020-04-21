37fbef0976bb3aa47f8d90885a16c36a305203a0
Yanwardo Sitanggang <ywdfsctgclev0301@gmail.com>
2020-04-21 16:04:13 +0700

    Model untuk notifikasi dibuat

---
+++ database/migrations/2020_04_21_152800_create_notifikasis_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotifikasisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*
+        notif_owner >> email pemilik email
+        notif_text >> teks notifikasi
+        notif_goto >> link rujukan
+        */
+
+         Schema::create('notifikasis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('notif_owner');
+            $table->string('notif_text');
+            $table->string('notif_goto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifikasis');
+    }
+}
