bc02507696b436683143f6e7f0f447abac0d0785
Yanwardo Sitanggang <ywdfsctgclev0301@gmail.com>
2020-04-26 02:05:02 +0700

    fitur grafik

--- resources/views/mahasiswa/beranda.blade.php
+++ resources/views/mahasiswa/beranda.blade.php
@@ -8,8 +8,8 @@
             <div class="col-sm float-left">
                 <div class="card mb-1">
                     <div class="card-body">
-                        Hari <br>
-                        30/50 <br>
+                        <div>Hari</div>
+                        <div  id="waktuTAV"></div>
                     </div>
                     <div class="card-header bg-light">
                         Waktu TA
@@ -27,9 +27,9 @@
                 </div>
             </div><div class="col-sm  float-left">
                 <div class="card mb-1">
-                    <div class="card-body">
-                        <br>
-                        {{$bimbingan}}
+                    <div class="card-body" id="jmlBimbinganV">
+                        <div id="jmlBimbinganV">{{$bimbingan->count()}}</div>
+                        <div>Kartu</div>
                     </div>
                     <div class="card-header bg-light">
                         Bimbingan
@@ -53,17 +53,92 @@
     </div>
 </div>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="{!! asset('assets/js/Chart.bundle.js') !!}"></script>
 <script src="{!! asset('assets/js/Chart.bundle.min.js') !!}"></script>
 <script type="text/javascript">
+
+
+    var masaTA = {!!json_encode($masaTA)!!};
+    masaTA = JSON.parse(masaTA);
+    
+    //Setup Variable
+    waktuSekarang = new Date();
+    waktuMulai = new Date(Date.parse(masaTA.mulai));
+    waktuSelesai = new Date(Date.parse(masaTA.selesai));
+    durasiT = waktuSelesai.getYear()-waktuMulai.getYear();
+    durasiB = (waktuSelesai.getMonth()-waktuMulai.getMonth()) + durasiT*12;
+    bulanMulai = waktuMulai.getMonth();
+
+
+    //Setup window Waktu TA
+    $("#waktuTAV").text(function(){
+        return (Math.round((waktuSekarang-waktuMulai)/86400000)).toString()+" / ".toString()+
+                (Math.round((waktuSelesai-waktuMulai)/86400000)).toString();
+    });
+
+
+    //setup Chart
+    listBulan=['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul',
+                'Agu', 'Sep', 'Okt', 'Nov', 'Des'];
+    
+    
+    durasiB+bulanMulai
+
+    bimbTimes = {!!json_encode($bimbinganTimes)!!};
+    
+    bimbTimes.forEach((e, i) => {
+        bimbTimes[i] = [parseInt(e.split('-')[0]), parseInt(e.split('-')[1])];
+    });
+    
+    tmpBimb = [];
+    for(i=0; i<=bimbTimes[bimbTimes.length-1][0]-bimbTimes[0][0]; i++){
+        tmpBimbT = [];
+        bimbTimes.forEach(e => {
+            if(e[0]==bimbTimes[i][0]){
+                tmpBimbT.push(e);
+
+            }
+        });
+        tmpBimb.push(tmpBimbT);
+
+    }
+    tmpBimbNew = [];
+    for(i=0; i<tmpBimb.length; i++){
+        tmpBimbNew = tmpBimbNew.concat(tmpBimb[i]);
+    }
+
+    jmlPBln = [];
+    for(i=bulanMulai; i<=durasiB+bulanMulai; i++){
+        jmlPBln.push([i,0]);
+    }
+
+    for(i=0; i<jmlPBln.length; i++){
+        for(j=0; j<tmpBimbNew.length; j++){
+            if(jmlPBln[i][0]==tmpBimbNew[j][1]){
+                jmlPBln[i][1]++;
+            }
+        }
+    }
+
+    chartData = [];
+    for(i=0; i<jmlPBln.length; i++){
+        chartData.push(jmlPBln[i][1]);
+    }
+
+    chartLabel=[''];
+    for(b=bulanMulai; b<=durasiB+bulanMulai; b++){
+        chartLabel.push(listBulan[b%12]);
+    }
+
     var ctx = document.getElementById('progresMhs');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            'labels':['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul'],
+            'labels':chartLabel,
             'datasets':[{
                 'label': 'Progress Bimbingan',
-                'data': [3, 5, 3, 6],
+                'data': chartData,
                 'fill': false,
                 'borderColor': 'rgb(75, 100, 100)',
                 'lineTension': 0.1
@@ -71,5 +146,7 @@
         },
         options: {}
     });
+
+    data=undefined;
 </script>
 @endsection
